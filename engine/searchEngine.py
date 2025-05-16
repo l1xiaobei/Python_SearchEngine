f73e9f0c32b75b64bd119a1213cef099a3c0fca6
xiaobei <l1xiaobei@qq.com>
2025-05-16 13:49:42 +0800

    ver 0.0.13

--- engine/searchEngine.py
+++ engine/searchEngine.py
@@ -1,35 +1,3 @@
-# from elasticsearch import Elasticsearch
-
-# class SearchEngine:
-#     def __init__(self, index_name="financial_articles"):
-#         """连接到 Elasticsearch 并指定索引"""
-#         self.es = Elasticsearch("http://localhost:9200")
-#         self.index_name = index_name
-
-#     def search(self, query):
-#         """在 Elasticsearch 进行搜索"""
-#         if not query:
-#             return []
-
-#         es_query = {
-#             "query": {
-#                 "multi_match": {
-#                     "query": query,
-#                     "fields": ["title", "content"]  # 在标题和正文中搜索
-#                 }
-#             }
-#         }
-
-#         response = self.es.search(index=self.index_name, body=es_query)
-
-#         results = []
-#         for hit in response["hits"]["hits"]:
-#             results.append({
-#                 "title": hit["_source"]["title"],
-#                 "url": hit["_source"]["url"],
-#                 "content": hit["_source"]["content"][:200] + "..."  # 显示前200个字符
-#             })
-#         return results
 from elasticsearch import Elasticsearch
 
 class SearchEngine:
@@ -51,9 +19,10 @@ class SearchEngine:
                             "query": query,
                             "fields": ["title^3", "content"]  # 标题加权
                         }
-                    },
-                    "boost_mode": "sum",
-                    "score_mode": "sum",
+                    },# 这个原始 _score 就是 Elasticsearch 的文本相关性得分。
+                    # "boost_mode": "sum",
+                    "boost_mode": "multiply", # 最终得分 = 文本匹配得分 × 时间得分
+                    "score_mode": "sum", # 合并多个function 但这里只有一个，故score_mode: "sum" 就是函数得分 = 时间得分
                     "functions": [
                         {
                             "gauss": {
@@ -71,7 +40,9 @@ class SearchEngine:
                 "pre_tags": ["<mark>"],
                 "post_tags": ["</mark>"],
                 "fields": {
-                    "title": {},
+                    "title": {
+
+                    },
                     "content": {
                         "fragment_size": 200,
                         "number_of_fragments": 1
@@ -90,6 +61,9 @@ class SearchEngine:
             results.append({
                 "title": highlight.get("title", [source["title"]])[0],
                 "url": source["url"],
-                "content": highlight.get("content", [source["content"][:200]])[0] + "..."
+                "content": highlight.get("content", [source["content"][:200]])[0] + "...",
+                "source": source.get("source", ""),
+                "timestamp": source.get("timestamp", "")[:10], # 只保留日期部分
+                "score": hit["_score"]  # [DEBUG] 查看相关性得分
             })
         return results
